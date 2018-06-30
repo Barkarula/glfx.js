7165bfa2546820507f4f51c037361533175e46f5
MadeByMike <mike@madebymike.com.au>
2018-06-30 18:18:37 +1000

    Add banner

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+/*
+ * glfx.js
+ * http://evanw.github.com/glfx.js/
+ *
+ * Copyright 2011 Evan Wallace
+ * Released under the MIT license
+ */
+
 import "./OES_texture_float_linear-polyfill";
 import { canvas } from "./core/canvas";
-
 export { canvas };
