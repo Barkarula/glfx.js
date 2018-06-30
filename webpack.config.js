7629a3ca55f6f7bbbb3cdbe5895698a00cbc7b2e
MadeByMike <mike@madebymike.com.au>
2018-06-30 18:03:49 +1000

    Add dist build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const webpack = require("webpack");
 
-module.exports = {
-  entry: ["./src/index.js"],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-source-map",
-  resolve: {
-    extensions: ["*", ".js"]
-  },
-  output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "glfx.js",
-    libraryTarget: "var",
-    library: "fx"
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devServer: {
-    contentBase: __dirname + "/tests",
-    hot: true
-  }
+module.exports = (env, argv) => {
+  return {
+    entry: ["./src/index.js"],
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    devtool: argv.mode === "production" ? false : "cheap-module-source-map",
+    resolve: {
+      extensions: ["*", ".js"]
+    },
+    output: {
+      path: __dirname + "/dist",
+      publicPath: "/",
+      filename: "glfx.js",
+      libraryTarget: "var",
+      library: "fx"
+    },
+    plugins:
+      argv.mode === "production"
+        ? []
+        : [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+      contentBase: __dirname + "/tests",
+      hot: true
+    }
+  };
 };
